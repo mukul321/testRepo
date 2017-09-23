324d40a12e562dde45ba13c067401ae4a2eff256
Nitin <Nitin@DESKTOP-T5SQPDO>
2017-09-23 08:38:01 +0530

    update 2

--- seleniumFramework/src/main/java/com/seleniumFramework/testBase/TestBase.java
+++ seleniumFramework/src/main/java/com/seleniumFramework/testBase/TestBase.java
@@ -1,5 +1,15 @@
 package com.seleniumFramework.testBase;
 
 public class TestBase {
+	
+	public void getBrowser(String browser) {
+		System.out.println(System.getProperty("os.name"));
+		if (System.getProperty("os.name").contains("Window")) {
+			if (browser.equalsIgnoreCase("firefox")) {
+				System.out.println(System.getProperty("user.dir"));
+				
+			}
+		}
+	}
 
 }
