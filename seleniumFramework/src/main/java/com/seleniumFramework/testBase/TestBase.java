7a5d794bddd0d0b6d85f9f2aceafb0e75e4be138
nitink007 <nitinkumar1992.delhi@gmail.com>
2017-09-23 11:15:50 +0530

    edit 4

--- seleniumFramework/src/main/java/com/seleniumFramework/testBase/TestBase.java
+++ seleniumFramework/src/main/java/com/seleniumFramework/testBase/TestBase.java
@@ -9,6 +9,7 @@ public class TestBase {
 	WebDriver driver;
 	
 	public void getBrowser(String browser) {
+		
 		System.out.println(System.getProperty("os.name"));
 		if (System.getProperty("os.name").contains("Window")) {
 			if (browser.equalsIgnoreCase("firefox")) {
@@ -22,6 +23,26 @@ public class TestBase {
 				driver = new ChromeDriver();
 			}
 		}
+		
+		else if (System.getProperty("os.name").contains("Mac")) {
+			if (browser.equalsIgnoreCase("firefox")) {
+				System.out.println(System.getProperty("user.dir"));
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver");
+				driver = new FirefoxDriver();
+			}
+			else if (browser.equalsIgnoreCase("chrome")) {
+				System.out.println(System.getProperty("user.dir"));
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/chromedriver");
+				driver = new ChromeDriver();
+			}
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		TestBase test = new TestBase();
+		test.getBrowser("chrome");
 	}
 
 }
